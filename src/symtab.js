9df114e64c6020d83494293c8582c7885ef99ab1
Mustafa Abukhadra <abukhadra@gmail.com>
2024-09-29 00:39:20 +0300

    update

--- src/symtab.js
+++ src/symtab.js
@@ -1,8 +1,8 @@
 const fns = []            // FIXME: workaround, storing fn names here , until name resolution  is complete.
 const structs = []          // FIXME: workaround , just like funcs
 
-function symtab_has_fn() { return fns}
-function symtab_has_struct() { return structs}
+function symtab_has_fn(fn) { return fns.includes(fn)}
+function symtab_has_struct(struct) { return structs.includes(struct)}
 
 function insert_symtab_fns(fn) { fns.push(fn)}
 function insert_symtab_structs(struct) { structs.push(struct) }
