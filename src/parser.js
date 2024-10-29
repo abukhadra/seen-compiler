8d8c0bc4b2320f91cc645835f3916845403cfbed
Mustafa Abukhadra <abukhadra@gmail.com>
2024-10-29 09:02:16 +0300

    update

--- src/parser.js
+++ src/parser.js
@@ -1312,9 +1312,9 @@ export default class Parser {
         const  maybe_children = () => {
             if(!this.is_dcolon()) { return }
             this.next()
+            const children = []
             if(this.is_open_curly()) { 
                 this.next()
-                const children = []
                 while (!(this.is_eof() || this.is_close_curly())) {
                     const expr = this.maybe_expr()  // FIXME: for typedef: this should be types not exprs
                     if(!expr) { break }
@@ -1322,7 +1322,6 @@ export default class Parser {
                     this.optional_comma()
                 }        
                 this.req_close_curly()
-                children.push()
                 return children
             } else {
                 children.push(this.req_expr())
